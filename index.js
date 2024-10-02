bfa127874e54a9dded12019cbe02d49130bbfd98
Nimrashoaib010 <shoaibnim010@gmail.com>
2024-10-03 00:33:48 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+var newpara = document.createElement("p");
+var text = document.createTextNode("Hello EveryOne!");
+newpara.appendChild(text);
+
+var div = document.createElement("div");
+var div1 = document.createTextNode("Hello div");
+div.appendChild(div1);
+
+var heading = document.createElement("h1");
+var head = document.createTextNode("Hello EveryOne!");
+newpara.appendChild(head);
+
+var newbutton = document.createElement("button");
+var btn = document.createTextNode("Click Me!");
+newbutton.appendChild(btn);
+
+var newUl = document.createElement("ul");
+var li = document.createTextNode("listItems");
+newUl.appendChild(li);
+
+var newlink = document.createElement("a");
+var link = document.createTextNode("Go to Google");
+newlink.appendChild(link);
+
+var newSpan = document.createElement("span");
+var text = document.createTextNode("Hello Span!");
+newSpan.appendChild(text);
+
+var newheader = document.createElement("h2");
+var header = document.createTextNode("This is h2 Heading!");
+newheader.appendChild(header);
+
+var newlabel = document.createElement("label");
+var text = document.createTextNode("userName");
+newlabel.appendChild(text);
+
+var footer = document.createElement("footer");
+var text = document.createTextNode("Go to Footer");
+footer.appendChild(text);
